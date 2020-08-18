311d07e26116f995a9080fcd88aeda3221c3f83a
Blast <Blast@Uber-Puter.lan>
2020-08-17 18:33:24 -0700

    BAGEL

---
+++ src/_05_for_loops/_1_my_ages/LoooooooooooopAndBadger.java
@@ -0,0 +1,15 @@
+package _05_for_loops._1_my_ages;
+
+public class LoooooooooooopAndBadger {
+public static void main(String[] args) {
+	for (int i = 0; i <= 1000; i++) {
+		System.out.println(i);
+	}
+	
+	
+	
+	
+	
+	
+}
+}
